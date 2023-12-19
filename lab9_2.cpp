aa6284c294b509f1a62685429df13f92b271fecf
RatchapornB <ratchaporn_b@cmu.ac.th>
2023-12-19 14:31:27 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 //Write the function printO() here
 
+void printO(int N,int M)
+{
+    if(N<=0 || M<=0){
+        cout << "Invalid input";
+    }else if(N>0){
+        int i = 0;
+        while(i<N){
+            if(M>0){
+                int j=0;
+                while(j<M){
+                    cout << "O";
+                    j++;
+                }
+                cout <<"\n";
+            }
+            i++;
+        }
+    }
+    
+}
+
+
 int main(){
 	
 	printO(2,2);
